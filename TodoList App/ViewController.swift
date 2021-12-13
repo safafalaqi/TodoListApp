381a4d0fc62580cf5e2f978e9400c4368b8ef00b
Safa Falaqi <sfalaqi@gmail.com>
2021-12-13 18:42:03 +0300

    Initial Commit

---
+++ TodoList App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList App
+//
+//  Created by Safa Falaqi on 13/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
